76513da918466b63663526d30dbcd087fbd01d4c
Ashley Maher <ashley.maher@didymodesigns.com.au>
2024-08-13 09:47:10 +1000

    adding the auhtentication end points for csrf and file upload.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cdd.didymodesigns.com.au/oauth/token',
+  csrfTokenUrl: "https://cdd.didymodesigns.com.au/session/token",
   surveyListURL: 'https://cdd.didymodesigns.com.au/rest/survey/list',
   getSurveyURL: 'https://cdd.didymodesigns.com.au/api/get-survey-resource/',
+  postUploadFileURL: 'https://cdd.didymodesigns.com.au/file/upload/survey_documents/_/file',
   clientId: 'your-client-id',
   clientSecret: 'your-client-secret'
 };
